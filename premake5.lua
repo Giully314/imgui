1ebe354442d14f0a701275a3abac8d397c97c7c7
Giully314 <giully@protonmail.com>
2022-06-15 22:47:26 +0200

    premake

---
+++ premake5.lua
@@ -0,0 +1,35 @@
+project "imgui"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "On"
+	systemversion "latest"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {"src/**.hpp", "src/**.cpp", "test/**.hpp", "test/**.cpp"}
+
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
